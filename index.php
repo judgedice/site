8009662578929caa762070b750bb0f0bdbdfc739
judgedice <judgedice@gmail.com>
2013-06-18 16:15:15 -0500

    added the vids

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
- phpinfo();
 
+// Nothing here.
 ?>
 
 <html>
@@ -11,8 +11,20 @@
 </head>
 
 <body>
+<h2>Some Videos</h2>
 
-<h3>Welcome to php on heroku!</h3>
+<iframe src="http://player.vimeo.com/video/68639818" width="1024" height="768" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+
+
+<iframe src="http://player.vimeo.com/video/68639813" width="1024" height="768" frameborder="1" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+
+
+<iframe src="http://player.vimeo.com/video/68639815" width="1024" height="768" frameborder="2" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+
+
+<iframe src="http://player.vimeo.com/video/68639814" width="1024" height="768" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+
+
 </body>
 
 </html>
